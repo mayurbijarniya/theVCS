4a6910dd8903c8dfa23a8b3b2fa04cf8488d2a07
MAYUR BIJARNIYA <mayur7041181394@gmail.com>
2023-01-12 15:49:52 +0530

    edited script

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-console.log("bullseye");
-console.log("mayur");
+// undefined
+var x;
+console.log("Value of x=" + x);
+
+// number
+x = 250;
+console.log("Value of x=" + x);
+
+// string
+str = "Hello All";
+console.log("Value of str=" + str);
+
+// boolean
+b = false;
+console.log("value of bool=" + b);
+
+// null -> intentional absence of any object value
+x = null;
+console.log("Value of x=" + x);
