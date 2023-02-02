0504a4d126caf750aae8ab0f721c6c87e6997d60
MAYUR BIJARNIYA <mayur7041181394@gmail.com>
2023-02-02 16:42:45 +0530

    react

--- react/first/src/App.js
+++ react/first/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    document.title = "You clicked ${counter} times";
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p> Your counter value is : {counter}</p>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Counter
+      </button>
     </div>
   );
 }
-
 export default App;
